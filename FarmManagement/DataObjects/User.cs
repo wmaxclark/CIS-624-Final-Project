97237fdead071336326a79ac481412f0abe8eab3
w.max.clark@gmail.com <w.max.clark@gmail.com>
2020-11-23 23:26:13 -0600

    Created program shell with necessary projects and references

---
+++ FarmManagement/DataObjects/User.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataObjects
+{
+    public class User
+    {
+    }
+}
---
+++ FarmManagement/Logic/IUserManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DataObjects;
+
+namespace Logic
+{
+    interface IUserManager
+    {
+        User AuthenticateUser(string username, string password);
+
+        bool UpdatePassword(string email, string oldPassword, string NewPassword);
+    }
+}
