4b67212f0868189c6294761d1c4820161b364a31
w.max.clark@gmail.com <w.max.clark@gmail.com>
2020-11-24 22:14:33 -0600

    Fixed Presentation layer namespace name

--- MarketGarden/MarketGarden/Images.Designer.cs
+++ MarketGarden/MarketGarden/Images.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MarketGarden {
+namespace PresentationLayer {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace MarketGarden {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MarketGarden.Images", typeof(Images).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PresentationLayer.Images", typeof(Images).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
